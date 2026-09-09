149aa9595fe509676f818465aace692d36de4dc6
mmrvega <mmr.mahmood@gmail.com>
2026-09-09 09:44:20 +0300

    adding pipeline update

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -6,20 +6,26 @@ echo   FoxPro to SQLite Migration Pipeline
 echo ==================================================
 
 :: 1- Run Extraction
-echo [1/3] Starting Extraction...
+echo [1/4] Starting Extraction...
 py extraction.py --all -w 21
 if %ERRORLEVEL% NEQ 0 goto :error
 
 echo.
 :: 2- Run Update All Columns
-echo [2/3] Updating all columns...
+echo [2/4] Updating all columns...
 py update_all_columns.py
 if %ERRORLEVEL% NEQ 0 goto :error
 
 echo.
 :: 3- Run Compare
-echo [3/3] Running comparison check...
+echo [3/4] Running comparison check...
 py compare.py
+if %ERRORLEVEL% NEQ 0 goto :error
+
+:: 4- run mssql compare 
+echo.
+echo [4/4] Running MSSQL comparison check...
+py mssql_compare.py --trusted --workers 8
 if %ERRORLEVEL% NEQ 0 goto :error
 
 echo.
